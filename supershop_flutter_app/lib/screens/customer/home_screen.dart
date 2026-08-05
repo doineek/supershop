07009b69e7690807c1811028b1d1e73f916922f0
doineek <doineek@example.com>
2026-08-05 17:44:25 +0600

    Fix LoginScreen constructor reference in home_screen.dart and compile clean release APK

--- supershop_flutter_app/lib/screens/customer/home_screen.dart
+++ supershop_flutter_app/lib/screens/customer/home_screen.dart
@@ -215,7 +215,7 @@ class _HomeScreenState extends State<HomeScreen> {
                         );
                         Navigator.push(
                           context,
-                          MaterialPageRoute(builder: (_) => const CustomerLoginScreen()),
+                          MaterialPageRoute(builder: (_) => const LoginScreen()),
                         );
                       } else {
                         showDialog(
